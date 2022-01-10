f6a4e02c55c567099f2fa19b5e53521350a2abd5
Jan Brombach <jan@brombach.de>
2022-01-10 15:13:40 +0100

    Added fluidTypeSystem through Calc function on body-attributtes

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 // Define Stuff
 
-const getValue = document.querySelectorAll('[data-custom-css]');
+const bodyData = document.body.dataset;
+
+const OPTIONS ={
+    minVw: bodyData.minVw,
+    maxVw: bodyData.maxVw,
+    minValue: bodyData.minValue,
+    maxValue: bodyData.maxValue
+}
+
+const fluidTypeSystem = ( data ) => {
+    return `
+        html {
+          font-size: ${data.minValue}px;
+        }
+        @media (min-width: ${data.minVw}px) {
+          html {
+            font-size: calc( ${data.minValue}px + (${data.maxValue} - ${data.minValue}) * (100vw - ${data.minVw}px) / (${data.maxVw} - ${data.minVw}) );
+          }
+        }
+        @media (min-width: ${data.maxVw}px) {
+          html {
+            font-size: ${data.maxValue}px;
+          }
+        }
+      }        
+    `;
+};
+
 let styles = document.createElement('style');
+styles.innerHTML = fluidTypeSystem( OPTIONS );
+
+const header = document.head;
+header.appendChild(styles);
 
 
-// Construct Stuff
+
+// Free Custom CSS with data-attribute
+const getValue = document.querySelectorAll('[data-custom-css]');
 
 [...getValue].forEach(element => {
     element.style.cssText = element.dataset.customCss;
 });
-
-
-// Init Stuff
-
-const header = document.head;
-header.appendChild(styles);
