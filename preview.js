aa6b6ce6017f8a97dad3e8b6ded92d932e43725e
Jan Brombach <jan@brombach.de>
2022-01-11 18:00:27 +0100

    add previewjs for chrome extension

---
+++ preview.js
@@ -0,0 +1,46 @@
+// Define Stuff
+
+const bodyData = document.querySelector(".body").dataset;
+
+const OPTIONS ={
+    minVw: bodyData.minVw,
+    maxVw: bodyData.maxVw,
+    minValue: bodyData.minValue,
+    maxValue: bodyData.maxValue
+}
+
+const fluidTypeSystem = ( data ) => {
+    return `
+        html {
+          font-size: ${data.minValue}px;
+        }
+        @media (min-width: ${data.minVw}px) {
+          html {
+            font-size: calc( ${data.minValue}px + (${data.maxValue} - ${data.minValue}) * (100vw - ${data.minVw}px) / (${data.maxVw} - ${data.minVw}) );
+          }
+        }
+        @media (min-width: ${data.maxVw}px) {
+          html {
+            font-size: ${data.maxValue}px;
+          }
+        }
+      }        
+    `;
+};
+
+let styles = document.createElement('style');
+styles.innerHTML = fluidTypeSystem( OPTIONS );
+
+const header = document.head;
+header.appendChild(styles);
+
+
+
+/*
+const getValue = document.querySelectorAll('[data-custom-css]');
+
+[...getValue].forEach(element => {
+    element.style.cssText = element.dataset.customCss;
+});
+
+*/
